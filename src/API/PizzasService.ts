6db38f50491089ac25a43d5f81624a1c197635a8
SashaGitHub01 <sashab4b1n@yandex.ru>
2021-11-06 11:20:57 +0300

    fix axios

--- src/API/PizzasService.ts
+++ src/API/PizzasService.ts
@@ -1,9 +1,8 @@
-import { instanse } from "./instanse";
+import axios from "axios";
 
 export class PizzasService {
    static getItems = async (filter: number, sort: string) => {
-      const res = await instanse
-         .get(`/pizzas?${filter ? `category=${filter}&` : ''}_sort=${sort}&_order=desc`);
+      const res = await axios.get(`/pizzas?${filter ? `category=${filter}&` : ''}_sort=${sort}&_order=desc`);
 
       return res.data;
    }
