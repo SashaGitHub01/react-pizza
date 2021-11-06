91dde2b449edeefe311feb52e1f244ff8f42996f
SashaGitHub01 <sashab4b1n@yandex.ru>
2021-11-06 12:09:25 +0300

    fixxxed

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -15,5 +14,3 @@ ReactDOM.render(
    </Provider>,
    document.getElementById('root')
 );
-
-reportWebVitals();
