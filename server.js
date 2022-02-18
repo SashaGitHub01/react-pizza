17e9d992cd80042a8d6266c55c0c71dbb1b70209
SashaGitHub01 <sashab4b1n@yandex.ru>
2022-02-18 14:09:29 +0300

    fix server 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({
-   static: '',
+   static: 'public',
 });
 
 const PORT = process.env.PORT || 3001;
@@ -11,7 +11,8 @@ server.use(middlewares);
 server.use(router);
 
 if (process.env.NODE_ENV === "production") {
-   server.use(express.static("build"));
+   server.use(jsonServer.defaults('build'));
+   server.use(jsonServer.router('build/db.json'));
 
    server.get("*", (req, res) =>
       res.sendFile(path.resolve("build", "index.html"))
